5c222a648f1221b2404f4f607028b64b4bc950a5
Davi da Silva Romão <davi.romao0210@gmail.com>
2022-05-04 15:24:57 -0300

    only current user can destroy/update your user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authorize_request, except: :create
-  before_action :find_user, except: %i[create index]
+  before_action :find_user, only: %i[ show ]
 
   def index
     @users = User.all
@@ -21,25 +21,25 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
-      render json: @user, status: :ok
+    if @current_user.update(user_params)
+      render json: @current_user, status: :ok
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @current_user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @user.destroy
-    render json: @user, status: :ok
-  end
-
-  def find_user
-    @user = User.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { errors: 'User not found' }, status: :not_found
+    @current_user.destroy
+    render json: @current_user, status: :ok
   end
 
   private
+    def find_user
+      @user = User.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { errors: 'User not found' }, status: :not_found
+    end
+
     def user_params
       params.permit(:email, :name, :password, :password_confirmation, :cpf)
     end
